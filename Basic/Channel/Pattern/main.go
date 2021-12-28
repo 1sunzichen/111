aebcabc217896897a080f54d11fd791be1a82271
孙家奇 <3023493319@qq.com>
2021-12-28 23:32:55 +0800

    'feat:優雅推出🤩'

--- Basic/Channel/Pattern/main.go
+++ Basic/Channel/Pattern/main.go
@@ -6,23 +6,110 @@ import (
 	"time"
 )
 
-func msgGen() chan string{
-	c:=make(chan string)
+func msgGen(name string) chan string {
+	c := make(chan string)
 	//go routine
-	go func(){
-        i:=0
-        for{//循环给数据
-        	time.Sleep(time.Duration(rand.Intn(2000))*time.Millisecond)
-        	c <-fmt.Sprintf("msg%d",i)
-        	i++
+	go func() {
+		i := 0
+		for { //循环给数据
+			time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
+			c <- fmt.Sprintf("msg%s %d", name, i)
+			i++
 		}
 	}()
 	return c
 }
+func msgGen2(name string, done chan struct{}) chan string {
+	c := make(chan string)
+	//go routine
+	go func() {
+		i := 0
+		for { //循环给数据
+			select {
+			case <-time.After(3 * time.Second):
+				c <- fmt.Sprintf("msg%s %d", name, i)
+			case <-done:
+				fmt.Println("clean up")
+				time.Sleep(2 * time.Second)
+				fmt.Println("clean updone")
+				done <- struct{}{}
+				return
+			}
+			time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
+			c <- fmt.Sprintf("msg%s %d", name, i)
+			i++
+		}
+	}()
+	return c
+}
+func fanIn(c1s ...chan string) chan string {
+	c := make(chan string)
+
+	for _, a := range c1s {
+		//a1:=a
+		go func(a1 chan string) {
+			for {
+				c <- <-a1
+			}
+		}(a)
+	}
+
+	return c
+}
+func fanInSelect(c1, c2 chan string) chan string {
+	c := make(chan string)
+	// 开一个协程
+	go func() {
+		for {
+
+			select {
+			case n := <-c1:
+				c <- n
+			case n := <-c2:
+				c <- n
+			}
+		}
+	}()
+
+	return c
+}
+
+//非阻塞等待
+func nonBlockingWait(c chan string) (string, bool) {
+	select {
+	case m := <-c:
+		return m, true
+	default:
+		return "", false
+	}
+
+}
+
+//超时
+func timeOutWait(c chan string, timeout time.Duration) (string, bool) {
+	select {
+	case m := <-c:
+		return m, true
+	case <-time.After(timeout):
+		return "", false
+	}
+}
 
 func main() {
-	m:=msgGen()
-	for{
-		fmt.Println(<-m)
+	done := make(chan struct{})
+	//m1 := msgGen("小郑")
+	m2 := msgGen2("小孙", done)
+	//m := fanIn(m1, m2)
+	//for {
+	//	fmt.Println(<-m)
+	//}
+	for i := 0; i < 6; i++ {
+		if a, ok := timeOutWait(m2, time.Second); ok {
+			fmt.Println(a)
+		} else {
+			fmt.Println("超时")
+		}
 	}
+	done <- struct{}{}
+	<-done
 }
